bbf0ec777ea0f2c927704e6f50bede70ed1a774d
zuohuaiyu1205 <56382951+zuohuaiyu1205@users.noreply.github.com>
2020-04-02 16:07:17 +0800

    Add files via upload

---
+++ Voip/Rtcp/RtcpPacket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Montage.Voip.Qos.Rtcp
+{
+    public abstract class RtcpPacket : DisposeObject
+    {
+        private uint version;
+        private bool padding;
+        private RtcpType packetType;
+
+        public uint Version
+        {
+            get { return version; }
+            set { version = value; }
+        }
+
+        public bool Padding
+        {
+            get { return padding; }
+            set { padding = value; }
+        }
+
+        public RtcpType PacketType
+        {
+            get { return packetType; }
+            set { packetType = value; }
+        }
+
+        public abstract void ToByteArray(byte[] buffer, ref uint offset);
+
+        public abstract void ParseData(byte[] buffer, ref uint offset);
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(string.Format("Version: {0}", this.Version));
+            stringBuilder.AppendLine(string.Format("Padding: {0}", this.Padding));
+            stringBuilder.AppendLine(string.Format("Type: {0}", this.PacketType));
+            return stringBuilder.ToString();
+        }
+
+        protected RtcpPacket()
+        {
+            this.Version = 2;
+            this.Padding = false;
+        }
+
+        ~RtcpPacket() { }
+    }
+}
